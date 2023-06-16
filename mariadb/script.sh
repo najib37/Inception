c293de16a1c43d0add756622e13c3f2fa2afadf7
najib37 <najibramadan37@gmail.com>
2023-06-16 15:01:50 +0000

    before adding the docker compose

---
+++ mariadb/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# create user $WP_USER@'localhost' identified by $WP_PASS;
+export DBROOTPASS="r1234" WP_USER="najib" WP_PASS="1234"
+sed -i 's/127.0.0.1/exec/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+service mysql start;
+mysql -e \
+	"create database IF NOT EXISTS wordpressdb;
+	grant all privileges on wordpressdb.* to '$WP_USER'@'wordpress.my_network' identified by '$WP_PASS';
+	flush privileges;"
+mysqladmin --user=root password "$DBROOTPASS"
+service mysql stop
+exec mysqld
---
+++ wordpress/wp-setup.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+// the website is wrong 
+// set password in 
+
+wp --allow-root config create --dbname=wordpressdb --dbuser=najib --dbpass=1234 --dbhost=mariadb.my_network --quiet
+wp --allow-root core install --url=165.22.17.126 --title="najib LLC" --admin_user="najib" --admin_password=1234 --admin_email=test@test.com --quiet
+exec php-fpm7.3 -F -R
